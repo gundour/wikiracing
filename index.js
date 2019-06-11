8408d01ac2dfe67038c73e2addb80c61e3ee05f4
Mohamed Gundour <mohamedgundour@gmail.com>
2019-06-12 00:53:48 +0200

    add article route

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const Game = require('./lib')
 let game = new Game('Wikipedia:Wiki Game', 'Austro-Hungarian Compromise of 1867')
 
 game.find()
-    .then(result => {
-        console.log(result);
+    .then(article => {
+        let route = article.getLinkRoute()
+        console.log('Article route', route);
     })
     .catch(err => {
         console.log(err);
