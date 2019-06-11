ec8225e9b5759a660971164efbde9790e3b56578
Mohamed Gundour <mohamedgundour@gmail.com>
2019-06-12 01:12:26 +0200

    handle errors

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ let game = new Game('Wikipedia:Wiki Game', 'Austro-Hungarian Compromise of 1867'
 game.find()
     .then(article => {
         let route = article.getLinkRoute()
-        console.log('Article route', route);
+        console.log('\n');
+        console.log('Article route:', route.join(' ---> '));
     })
     .catch(err => {
         console.log(err);
