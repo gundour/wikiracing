fd40a7013e8b0ecb404fb38ec34fbf7e973acc90
Mohamed Gundour <mohamedgundour@gmail.com>
2019-06-11 23:51:22 +0200

    use wikipedia API and promise pipes

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const Game = require('./lib')
 
-let game = new Game('Wikipedia:Wiki_Game', 'World_Wide_Web')
+let game = new Game('Wikipedia:Wiki Game', 'Austro-Hungarian Compromise of 1867')
 
-game.orchestrate()
-
-
-game.on('data', data => {
-    console.log(data);
-    data.tree.print()
-})
-
-game.on('error', err => {
-    console.log(err);
-})
+game.find()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => {
+        console.log(err);
+    })
